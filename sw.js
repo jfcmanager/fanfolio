24ffe2d2ea814fec892c93ab236b10c1cca0a647
jfcmanager <lottiechan.jp@gmail.com>
2026-08-15 06:43:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fanfolio-v0.22.1';
+const CACHE_NAME = 'fanfolio-v0.22.2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
